17923e1a178a9fc134621486af18bf80566f03a7
venimecadesarrollo <Venimecacreativo1@gmail.com>
2025-09-27 17:05:41 -0400

    agregando post new

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 """def post_list(request):
     return render(request, 'blog.html')"""
@@ -15,3 +16,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class PostNewView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title','body', 'author']
+    
