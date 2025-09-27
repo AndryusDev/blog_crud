fc1bd24d901beff706adce22a6ffb15e48dd7014
venimecadesarrollo <Venimecacreativo1@gmail.com>
2025-09-27 15:29:17 -0400

    cambiando algunas cosas

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="PostListView"),
+    path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
 ]
