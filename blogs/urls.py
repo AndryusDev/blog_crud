4c83418b51ec02b8375af374b3ce0fba90d575a7
venimecadesarrollo <Venimecacreativo1@gmail.com>
2025-10-04 16:30:53 -0400

    colocando funcion de eliminar

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView,PostNewView,PostUpdateView
+from .views import PostListView, PostDetailView,PostNewView,PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostNewView.as_view(), name="post-new"),
-    path("post/<int:pk>/editar",PostUpdateView.as_view(), name="post-update")
+    path("post/<int:pk>/editar",PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/borrar",PostDeleteView.as_view(), name="post-delete"),
     ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 
 """def post_list(request):
     return render(request, 'blog.html')"""
@@ -27,3 +29,8 @@ class PostUpdateView(UpdateView):
     template_name = 'post_update.html'
     fields = ['title', 'body']
     
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post-list')
